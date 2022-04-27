441d070ce013f12e388f3553be94916a860c832e
sehoon27@naver.com <sehoon27@naver.com>
2022-04-27 17:53:50 +0900

    딜러 구현중

--- BlackJackGame_project/BlackJack.cpp
+++ BlackJackGame_project/BlackJack.cpp
@@ -4,11 +4,13 @@
 void MainGame(PLAYER& user, int batting)
 {
 	srand((unsigned int)time(NULL));
+	Dealer deal;
 	user.Gameset();
 	cout << " 딜러가 카드를 나누어준다. " << '\n' << '\n';
 	user.GetCard(rand() % 13 + 1, rand() % 4 + 1);
 	user.GetCard(rand() % 13 + 1, rand() % 4 + 1);
-	cout << " 현재 당신의 카드의 총 합은 " << user.Getnumber() << "입니다." << '\n';
+	deal.GetCard(rand() % 13 + 1, rand() % 4 + 1);
+	cout << " 현재 딜러의 카드의 총 합은 " << deal.Getnumber() << "입니다." << '\n';
 	while (1)
 	{
 		int select;
@@ -31,8 +33,27 @@ void MainGame(PLAYER& user, int batting)
 			user.Gamewin(batting);
 			return;
 		}
+		else if (deal.Getnumber() == 21)
+		{
+			cout << " 딜러의 카드의 총 합이 21으로 당신이 패배하였습니다." << '\n' << '\n';
+			cout << " 다음게임에서 행운이 있길..." << '\n' << '\n';
+			cout << " ★아무키나 누르세요★";
+			_getch();
+			user.Gamelose(batting);
+		}
 		else
 		{
+			if(deal.Getnumber()<17)
+			if (deal.Getnumber() == 21)
+			{
+				cout << " 딜러의 카드의 총 합이 21으로 당신이 패배하였습니다." << '\n' << '\n';
+				cout << " 다음게임에서 행운이 있길..." << '\n' << '\n';
+				cout << " ★아무키나 누르세요★";
+				_getch();
+				user.Gamelose(batting);
+			}
+			
+
 			while (1)
 			{
 				cout << " 당신의 카드의 총 합은 " << user.Getnumber() << " 입니다." << '\n' << '\n';
